9d5994ac3511a9c598ac637a1d582fe37add311f
FrancescoDOrtona <francescod.97@hotmail.it>
2023-10-23 14:11:51 +0200

    Add: Print title with vue & img

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const createApp = Vue.createApp
+createApp({
+    data() {
+        return {
+            message: 'Start with Vue!',
+            image: 'https://www.civitatis.com/blog/wp-content/uploads/2022/11/calle-akihabara-tokio.jpg'
+        }
+    }
+}).mount('#app')
+console.log(this)
