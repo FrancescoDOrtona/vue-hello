973ed1c548b5bec426b7b7514dde22fd5722e0b3
FrancescoDOrtona <francescod.97@hotmail.it>
2023-10-23 14:39:05 +0200

    Add: Vue practice & some CSS style

--- js/app.js
+++ js/app.js
@@ -3,7 +3,18 @@ createApp({
     data() {
         return {
             message: 'Start with Vue!',
-            image: 'https://www.civitatis.com/blog/wp-content/uploads/2022/11/calle-akihabara-tokio.jpg'
+            image: 'https://www.civitatis.com/blog/wp-content/uploads/2022/11/calle-akihabara-tokio.jpg',
+            guessText: '',
+            counter: 0
+        }
+    },
+    methods: {
+        onClick: function (){
+            this.counter++;
+            console.log(this.counter);
+        },
+        onClickReset: function (){
+            this.counter = 0;
         }
     }
 }).mount('#app')
